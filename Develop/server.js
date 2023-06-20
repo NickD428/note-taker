88ed50ea511c79639794163046ddb4e0821b2c24
Nick <chargerfan428@aol.com>
2023-06-19 19:49:56 -0700

    added express functions and html routes

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
