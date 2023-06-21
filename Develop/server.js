fdb483cdf288160e6cfc55cf7458caa7ddd1ef13
Nick <chargerfan428@aol.com>
2023-06-20 20:31:02 -0700

    added const variable for uuid

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
